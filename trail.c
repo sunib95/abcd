fbe2a41a410a207e5aad4053f25fd4790bb15a90
sunib95 <behera.sunita01@gmail.com>
2017-05-09 02:36:44 +0530

    for loop fix

--- trail.c
+++ trail.c
@@ -6,12 +6,14 @@ int main()
 {
    int i;
     for(i=1; i<=100;i++ )
+    {
     if(i%2==0)
     printf("the even number is %d\n",i);
     else if(i%2==1)
     printf("the odd number is %d\n",i);
     else
-        printf("shit");
+        printf("quit");
+}
 
    return 0;
 }
